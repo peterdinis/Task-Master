49136e9aae0dc7dc6afdf81ea136af7098affff8
Peter Dinis <peterdinis0@pobox.sk>
2024-10-20 12:33:10 +0200

    feat: Small change in hono routes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,21 +1,13 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
 import loginRoute from '../login/route';
-import registerRoute from '../register/route';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
-app.get('/hello', (c) => {
-	return c.json({
-		message: 'Hello Next.js!',
-	});
-});
-
 const routes = 
 	app.route("/auth/login", loginRoute)
-	.route("/auth/register", registerRoute)
 
 
 export const GET = handle(app);
